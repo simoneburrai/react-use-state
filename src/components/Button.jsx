cd75407c0d6f1114ee1d8b0ff7ab766cda9e32de
simoneburrai <burrai.s98@gmail.com>
2025-04-09 10:24:44 +0200

    feat: removed useState from button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,7 @@ import { useState } from "react";
 
 function Button({ title, onToggle, id, active }) {
     const isActive = active === true ? "active" : ""
-    const [isClicked, setIsClicked] = useState(false);
     const toggle = () => {
-        setIsClicked(prev => {
-            return !prev;
-        })
-        console.log("bottone cliccato")
-        console.log(isClicked);
         onToggle(id);
 
     }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,8 @@ const Card = ({ languages, languagesValue }) => {
     let header = undefined;
     let content = "Nessun Bottone Attivo";
     if (languagesValue != null) {
-        const currentLanguage = languages.find(language => language.id === languagesValue)
+        const currentLanguage = languages.find(language =>
+            language.id === languagesValue)
         if (currentLanguage) {
             header = <h2>{currentLanguage.title}</h2>;
             content = currentLanguage.description;
