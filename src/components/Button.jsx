c49206481a730db28888fbea5d195dfa31f7e410
simoneburrai <burrai.s98@gmail.com>
2025-04-09 11:23:36 +0200

    feat: removed useState for class active in the bottom, used check with language

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
-function Button({ onToggle, language, active }) {
-    const isActive = active === true ? "active" : ""
+function Button({ onToggle, language, className }) {
     const toggle = () => {
         onToggle(language);
 
     }
-    return <button className={isActive} onClick={toggle}>{language.title}</button>
+    return <button
+        className={className}
+        onClick={toggle}
+    >{language.title}</button>
 }
 
 export default Button;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,22 +4,17 @@ import Card from "./Card";
 import { useState } from "react";
 
 const Main = () => {
-    const [active, setActive] = useState(false);
     const [clickedLanguage, setClickedLanguage] = useState(null);
     const languageClicked = (obj) => {
         setClickedLanguage(clickedLanguage === obj ? null : obj);
-        setActive(active => {
-            active = false;
-            return active
-        });
     }
     return <main>
         <div className="button-container">
             {languages.map(language => <Button
                 language={language}
                 key={language.id}
-                active={clickedLanguage === language}
                 onToggle={languageClicked}
+                className={clickedLanguage && clickedLanguage.id === language.id ? "active" : ""}
             />)}
         </div>
         <Card language={clickedLanguage}>
